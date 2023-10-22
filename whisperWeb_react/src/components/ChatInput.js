7a6f1419eac8c12c240389280a1fe2caec7e6695
Jonathan Wad Høgsbro <jwh.it@cbs.dk>
2023-10-22 20:15:01 +0200

    converting into react

---
+++ whisperWeb_react/src/components/ChatInput.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ChatInput = () => {
+  return (
+    <input type="text" className="chatInput" placeholder="Type something..." />
+  );
+};
+
+export default ChatInput;
---
+++ whisperWeb_react/src/components/NavigationBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavigationBar = () => {
+  return (
+    <nav className="bottom-nav">
+      {/* Add your navigation links or buttons here */}
+    </nav>
+  );
+};
+
+export default NavigationBar;
