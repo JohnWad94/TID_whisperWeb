5a1b4068fc56a22fde85cd83d6e256eff4776173
JohnWad94 <47334557+JohnWad94@users.noreply.github.com>
2023-10-22 21:59:29 +0200

    progress

--- whisperweb/src/components/NavigationBar.js
+++ whisperweb/src/components/NavigationBar.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-const NavigationBar = () => {
+const NavigationBar = ({ activeTab }) => {
   return (
     <nav className="bottom-nav">
-      {/* Add your navigation links or buttons here */}
+      <a href="topics.html" className={`nav-item ${activeTab === 'topics' ? 'active' : ''}`}>
+        <img src="/images/New Chat chosen.png" alt="Image 1" className="nav-icon" />
+      </a>
+      <a href="index.html" className="nav-item">
+        <img src="/images/Chats.png" alt="Image 2" className="nav-icon" />
+      </a>
+      <a href="information.html" className="nav-item">
+        <img src="/images/Information.png" alt="Image 3" className="nav-icon" />
+      </a>
     </nav>
   );
 };
